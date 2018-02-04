3c93aa0f18f224adbd852a5031ae57d419fb1dbd
Limi <pklimi@163.com>
2018-02-05 00:07:06 +0800

    初始化辅助工程

---
+++ src/main/java/com/limi/jumper/JumpMain.java
@@ -0,0 +1,167 @@
+package com.limi.jumper;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import com.limi.jumper.JumpUi.UiListener;
+
+public class JumpMain {
+
+	private static String sdPath = "/sdcard/screenshot/jump.png";
+	private static String pcPath1 = "screenshot/jump_1.png";
+	private static String pcPath2 = "screenshot/jump_2.png";
+	private static boolean isThreadRunning = true;
+
+	public static void main(String[] args) {
+
+		// adbScreenShot();
+		// long mills = 50;
+		// swipe(50, 250, 250, 250, mills);
+		// log("Íê³É");
+
+		JumpUi jumpUi = new JumpUi();
+		jumpUi.setListener(createListener());
+		jumpUi.setScale(0.3);
+		jumpUi.setImage(pcPath2);
+		jumpUi.show();
+
+		ScreenShotThread thread = new ScreenShotThread();
+		thread.setJumpUi(jumpUi);
+		thread.start();
+	}
+
+	private static UiListener createListener() {
+
+		return new UiListener() {
+
+			public void onRange(JumpUi jumpUi, int x1, int y1, int x2, int y2) {
+
+				int dx = x2 - x1;
+				int dy = y2 - y1;
+				log("[onRange] " + String.format("%s,%s", dx, dy));
+
+				if (Math.abs(dx) > 30 && Math.abs(dy) > 30) {
+
+					swipe(x1, y1, x2, y2);
+				}
+			}
+
+			public void onClose() {
+
+				isThreadRunning = false;
+			}
+		};
+	}
+
+	private static void swipe(int x1, int y1, int x2, int y2) {
+
+		int dx = x2 - x1;
+		int dy = y2 - y1;
+		double range = Math.sqrt(dx * dx + dy * dy);
+		double rate = 1.41;
+		long mills = (long) (range * rate);
+		log("[swipe] " + mills + "ms");
+		String swipeCmd = String.format("shell input swipe %s %s %s %s %s", x1, y1, x2, y2, mills);
+		adbCommand(swipeCmd);
+	}
+
+	private static void adbScreenShot() {
+
+		String shotCmd = String.format("shell /system/bin/screencap -p %s", sdPath);
+		String pullCmd = String.format("pull %s %s", sdPath, pcPath1);
+
+		adbCommand(shotCmd);
+		adbCommand(pullCmd);
+	}
+
+	private static String adbCommand(String adbCmd) {
+
+		String adbPath = "E:/tools/android-sdk-windows/platform-tools/adb.exe";
+		return runCommand(String.format("%s %s", adbPath, adbCmd));
+	}
+
+	private static String runCommand(String cmd) {
+
+		log("[cmd] " + cmd);
+		String result = null;
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(Runtime.getRuntime().exec(cmd).getInputStream()));
+			String line = null;
+			StringBuffer b = new StringBuffer();
+			while ((line = br.readLine()) != null) {
+				b.append(line + "\n");
+			}
+			result = b.toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		log("[cmd] " + result);
+		return result;
+	}
+
+	private static void copyScreenShot() {
+
+		try {
+
+			InputStream inputStream = new FileInputStream(pcPath1);
+			OutputStream outputStream = new FileOutputStream(pcPath2);
+			byte[] buff = new byte[1024];
+			int len = 1024;
+			while ((len = inputStream.read(buff, 0, len)) != -1) {
+
+				outputStream.write(buff, 0, len);
+			}
+			inputStream.close();
+			outputStream.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static class ScreenShotThread extends Thread {
+
+		private JumpUi jumpUi;
+
+		@Override
+		public void run() {
+			super.run();
+
+			while (isThreadRunning) {
+
+				adbScreenShot();
+				copyScreenShot();
+				jumpUi.refresh();
+				try {
+					Thread.sleep(100);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public void setJumpUi(JumpUi jumpUi) {
+			this.jumpUi = jumpUi;
+		}
+	}
+
+	private static void log(Object log) {
+
+		String logStr = String.valueOf(log);
+		if (logStr.startsWith("[cmd]")) {
+			return;
+		}
+		if (logStr.startsWith("[onRange]")) {
+			return;
+		}
+
+		System.out.println(logStr);
+	}
+}
---
+++ src/main/java/com/limi/jumper/JumpUi.java
@@ -0,0 +1,189 @@
+package com.limi.jumper;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.RandomAccessFile;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class JumpUi {
+
+	private UiListener uiListener;
+	private double scale = 1;
+	private JPanel imagePanel;
+	private String imagePath;
+	byte[] cachedImage = null;
+	long lastModify = 0;
+
+	public void setListener(UiListener onRange) {
+
+		this.uiListener = onRange;
+	}
+
+	public void setScale(double scale) {
+
+		this.scale = scale;
+	}
+
+	public void refresh() {
+
+		if (imagePanel != null) {
+
+			imagePanel.repaint();
+			// imagePanel.updateUI();
+		}
+	}
+
+	public void show() {
+
+		// 显示应用 GUI
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	private void createAndShowGUI() {
+
+		// 确保一个漂亮的外观风格
+		JFrame.setDefaultLookAndFeelDecorated(true);
+
+		// 创建 JFrame 实例
+		final JFrame frame = new JFrame("Login Example");
+		// Setting the width and height of frame
+		// frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+
+				uiListener.onClose();
+			}
+		});
+
+		imagePanel = new JPanel() {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			private int count = 1;
+
+			protected void paintComponent(Graphics g) {
+
+				// if (!isImageChanged()) {
+				// super.paintComponent(g);
+				// return;
+				// }
+				System.out.println("paint: " + count);
+				count++;
+				try {
+					byte[] imageBytes = readFile(imagePath);
+					ImageIcon icon = new ImageIcon(imageBytes);
+					Image img = icon.getImage();
+					int width = (int) (icon.getIconWidth() * scale);
+					int height = (int) (icon.getIconHeight() * scale);
+					g.drawImage(img, 0, 0, width, height, icon.getImageObserver());
+					frame.setSize(width, height);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		};
+
+		imagePanel.addMouseListener(new MouseAdapter() {
+
+			private int x1;
+			private int y1;
+			private int x2;
+			private int y2;
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+				x1 = e.getX();
+				y1 = e.getY();
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+
+				x2 = e.getX();
+				y2 = e.getY();
+
+				if (uiListener != null) {
+
+					// onRange.onRange(JumpUi.this, x1, y1, x2, y2);
+					uiListener.onRange(JumpUi.this, (int) (x1 / scale), (int) (y1 / scale), (int) (x2 / scale),
+							(int) (y2 / scale));
+				}
+			}
+		});
+
+		frame.getContentPane().add(imagePanel);
+
+		frame.pack();
+		// 设置界面可见
+		frame.setVisible(true);
+	}
+
+	public interface UiListener {
+
+		void onRange(JumpUi jumpUi, int x1, int y1, int x2, int y2);
+
+		void onClose();
+	}
+
+	public void setImage(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	private boolean isImageChanged() {
+
+		if (cachedImage != null) {
+
+			File file = new File(imagePath);
+			if (file.lastModified() <= lastModify) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private byte[] readFile(String imagePath) {
+
+		if (!isImageChanged()) {
+			return cachedImage;
+		}
+
+		{
+			File file = new File(imagePath);
+			lastModify = file.lastModified();
+		}
+
+		byte[] result = null;
+		try {
+			RandomAccessFile file = new RandomAccessFile(imagePath, "r");
+			result = new byte[(int) file.length()];
+			file.read(result);
+			file.close();
+			cachedImage = result;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+}
